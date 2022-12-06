36d68d934b99cb99b5d94c1ba52e04eb8728e44f
ZeroK ZK <Jose.GeeksJose@gmail.com>
2022-12-06 01:11:09 -0500

    landing-page

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search",methods=['POST', 'GET'])
+def search():
+    searchName = request.args.get("search")
+    if request.method == 'POST':
+        return "Post"
+    return render_template("/layout/search.html")
+        
+    
+
+app.run(port=3000,debug=True)
