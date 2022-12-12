a84381335769d7953a44a3a75d62f58f23ed56ef
ZeroK ZK <Jose.GeeksJose@gmail.com>
2022-12-12 00:30:48 -0500

    tienda 1.0 terminada

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def login():
     cursor.execute("SELECT * FROM usuario WHERE email = '{}' AND hashPassword = '{}'".format(email.lower(), password.lower()))
     exist = cursor.fetchone()
     if exist:
-        response =  make_response(redirect("/cms-admin",200))
+        response =  make_response(redirect("/cms-admin"))
         response.set_cookie("token",str(exist[0]))
     else:
         response =  make_response("fail login", 401)
@@ -63,7 +63,7 @@ def admin():
         cursor = conetionDB.cursor()
         cursor.execute("SELECT * FROM usuario WHERE id = {}".format(isAdmin))
         data = cursor.fetchone()
-        cursor.execute("SELECT * FROM product LIMIT 50")
+        cursor.execute("SELECT * FROM product ORDER BY id DESC LIMIT 50 ")
 
         return render_template("/layout/dashboard.html",profile=data, products= cursor)
     else:
@@ -76,9 +76,54 @@ def dashBoardProducts():
     if isAdmin:
         conetionDB = conection()
         cursor = conetionDB.cursor()
-        cursor.execute("SELECT * FROM product LIMIT 50")
+        cursor.execute("SELECT * FROM product ORDER BY id DESC LIMIT 50")
         return render_template("/layout/dashBoardProducts.html", products=cursor)
     else:
         return render_template("/layout/login.html")
+@app.route("/cms-admin/product/edit/<string:id>",methods=['POST', 'GET'])
+def productEdit(id):
+    conetionDB = conection()
+    cursor = conetionDB.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM product WHERE id={}".format(id))
+        data = cursor.fetchone()
+        return render_template("layout/editProduct.html",infoProduct=data)
+    elif request.method == 'POST':
+        nombre = request.form['nombre']
+        desc = request.form['desc']
+        color = request.form['color']
+        marca = request.form['marca']
+        url = request.form['url']
+        price = request.form['price']
+        data = (nombre,desc,color,marca,url,price)
+        cursor.execute("UPDATE product SET nombre=%s,descripcion=%s,color=%s,brand=%s,imgUrl=%s,originalPrice=%s WHERE id = {}".format(id),data)
+        conetionDB.commit()
+        return redirect("/cms-admin/products")
 
+@app.route("/cms-admin/product/delete/<string:id>")
+def productDelete(id):
+    conetionDB = conection()
+    cursor = conetionDB.cursor()
+    cursor.execute("DELETE FROM product WHERE id={}".format(id))
+    conetionDB.commit()
+    return redirect("/cms-admin/products")
+@app.route("/cms-admin/product/add",methods=['POST', 'GET'])
+def productAdd():
+    if request.method == 'GET':
+        return render_template("/layout/addProduct.html")
+    elif request.method == 'POST':
+        conetionDB = conection()
+        cursor = conetionDB.cursor()
+        nombre = request.form['nombre']
+        desc = request.form['desc']
+        color = request.form['color']
+        marca = request.form['marca']
+        url = request.form['url']
+        originalPrice = request.form['originalPrice']
+        discountPrice = request.form['discountPrice']
+        categoria = request.form['categoria']
+        data = (nombre,desc,originalPrice,categoria,color,url,marca,discountPrice)
+        cursor.execute("INSERT INTO `product` (`id`, `nombre`, `descripcion`, `originalPrice`, `idCategory`, `color`, `imgUrl`, `brand`, `discountPrice`, `createAt`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, current_timestamp())",data)
+        conetionDB.commit()
+        return redirect("/cms-admin/products")
 app.run(port=3000,debug=True)
